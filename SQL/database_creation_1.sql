0d9a5b81e142eb65a89b7b18177b7ca68e39741c
Mayer <mayer.scharlat@gmail.com>
2024-08-26 20:25:44 -0400

    Separating the creation steps into 2 to avoid erros
    Splitting the creation of the data model into two separate files to allow for the insertion of data

--- SQL/database_creation_1.sql
+++ SQL/database_creation_1.sql
@@ -136,28 +136,5 @@ AFTER INSERT ON Orders
 FOR EACH ROW
 EXECUTE FUNCTION update_total_spent_func();
 
-------RUN AFTER DATA IS IMPORTED------
-
-ALTER TABLE Customer ADD CONSTRAINT fk_Customer_address_id FOREIGN KEY(address_id)
-REFERENCES Address (address_id);
-
-ALTER TABLE Customer ADD CONSTRAINT fk_Customer_member_id FOREIGN KEY(member_id)
-REFERENCES Membership (member_id);
-
-ALTER TABLE Address ADD CONSTRAINT fk_Address_customer_id FOREIGN KEY(customer_id)
-REFERENCES Customer (customer_id);
-
-ALTER TABLE Orders ADD CONSTRAINT fk_Order_customer_id FOREIGN KEY(customer_id)
-REFERENCES Customer (customer_id);
-
-ALTER TABLE OrderLine ADD CONSTRAINT fk_OrderLine_order_id FOREIGN KEY(order_id)
-REFERENCES Orders (order_id);
-
-ALTER TABLE OrderLine ADD CONSTRAINT fk_OrderLine_product_id FOREIGN KEY(product_id)
-REFERENCES Product (product_id);
-
-ALTER TABLE Membership ADD CONSTRAINT fk_Membership_customer_id FOREIGN KEY(customer_id)
-REFERENCES Customer (customer_id);
-
 ALTER TABLE Costs ADD CONSTRAINT fk_Costs_campaign_id FOREIGN KEY(campaign_id)
 REFERENCES Campaign (campaign_id);
