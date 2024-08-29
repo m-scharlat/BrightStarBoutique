efa4479924d3d68649419861197ec1d2931680cc
Mayer <mayer.scharlat@gmail.com>
2024-08-28 21:45:45 -0400

    Update business_analysis.sql

--- business_analysis.sql
+++ business_analysis.sql
@@ -1,4 +1,3 @@
-
 ---------------------------------------------
 -- KPI Driven Business Analysis
 ---------------------------------------------
@@ -22,6 +21,11 @@ Dimensions: Product Categories/Sub-Categories, Customer Demographics (location, 
 */
 
 -- :: Sales Volume ::
+
+-- Total Sales Volume
+SELECT
+	SUM(quantity) AS total_quantity
+FROM orderline;
 
 -- By Product Categories
 SELECT
@@ -74,11 +78,11 @@ SELECT
 	MAX(AGE), 
 	AVG(age) AS avg_age
 FROM
-(SELECT DATE_PART('year', AGE(NOW(),birthday)) as age FROM customer);
+(SELECT DATE_PART('year', AGE(NOW(),birthday)) AS age FROM customer);
 
 -- By all ages
 SELECT 
-	DATE_PART('year', AGE(NOW(),c.birthday)) as customer_age,
+	DATE_PART('year', AGE(NOW(),c.birthday)) AS customer_age,
 	SUM(oline.quantity) AS total_quantity
 FROM orders AS o
 LEFT JOIN orderline AS oline
@@ -116,34 +120,310 @@ GROUP BY c.gender
 ORDER BY c.gender DESC;
 
 -- By Customer Membership Status
+SELECT 
+	CASE
+		WHEN c.is_member = TRUE THEN 'Members'
+		ELSE 'Non-members'
+	END as member_status,
+	SUM(oline.quantity) AS total_quantity
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+LEFT JOIN customer AS c
+	USING(customer_id)
+GROUP BY member_status;
 
 -- By Year
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	SUM(oline.quantity) AS total_quantity
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year
+ORDER BY year ASC;
+
+-- Yearly Growth Rate
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	ROUND((SUM(oline.quantity) - LAG(SUM(oline.quantity)) OVER (ORDER BY DATE_PART('year',o.order_date)))/ CAST(LAG(SUM(oline.quantity),1) OVER (ORDER BY DATE_PART('year',o.order_date)) AS DECIMAL)* 100,2) AS q_yearly_growth_rate
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year;
+
+-- Average Yearly Growth Rate
+SELECT ROUND(AVG(q_yearly_growth_rate),2) AS avg_yearly_growth_rate
+FROM
+(
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	ROUND((SUM(oline.quantity) - LAG(SUM(oline.quantity)) OVER (ORDER BY DATE_PART('year',o.order_date)))/ CAST(LAG(SUM(oline.quantity),1) OVER (ORDER BY DATE_PART('year',o.order_date)) AS DECIMAL)* 100,2) AS q_yearly_growth_rate
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year
+);
 
 -- By Months
+SELECT 
+	DATE_PART('year',o.order_date) AS year,
+	DATE_PART('month',o.order_date) AS month,
+	SUM(oline.quantity) AS total_quantity
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year, month
+ORDER BY year ASC, month ASC;
+
+-- Monthly Growth Rate
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	DATE_PART('month',o.order_date) AS month,
+	ROUND((SUM(oline.quantity) - LAG(SUM(oline.quantity)) OVER (ORDER BY DATE_PART('month',o.order_date)))/ CAST(LAG(SUM(oline.quantity),1) OVER (ORDER BY DATE_PART('month',o.order_date)) AS DECIMAL)* 100,2) AS monthly_growth_rate
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year,month;
+
+-- Average Monthly Growth Rate
+SELECT ROUND(AVG(monthly_growth_rate),2) AS avg_monthly_growth_rate
+FROM
+(
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	DATE_PART('month',o.order_date) AS month,
+	ROUND((SUM(oline.quantity) - LAG(SUM(oline.quantity)) OVER (ORDER BY DATE_PART('month',o.order_date)))/ CAST(LAG(SUM(oline.quantity),1) OVER (ORDER BY DATE_PART('month',o.order_date)) AS DECIMAL)* 100,2) AS monthly_growth_rate
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year,month
+);
 
 -- :: Revenue ::
 
+-- Total Revenue 
+SELECT
+	SUM(line_total) AS total_revenue
+FROM orderline;
+
 -- By Product Categories
+SELECT
+	prod.category,
+	SUM(oline.line_total) AS total_revenue
+FROM orderline AS oline
+LEFT JOIN product AS prod
+	USING(product_id)
+GROUP BY prod.category;
 
 -- By Product Sub-Categories
+SELECT
+	prod.sub_category,
+	SUM(oline.line_total) AS total_revenue
+FROM orderline AS oline
+LEFT JOIN product AS prod
+	USING(product_id)
+GROUP BY prod.sub_category;
 
--- By Customer Location
+-- By Product Category & Sub-Category
+SELECT
+	prod.category,
+	prod.sub_category,
+	SUM(oline.line_total) AS total_revenue
+FROM orderline AS oline
+LEFT JOIN product AS prod
+	USING(product_id)
+GROUP BY prod.category, prod.sub_category
+ORDER BY prod.category ASC, total_revenue DESC;
 
--- By Customer Age
+-- By Customer Location (ie state)
+SELECT 
+	add.state,
+	SUM(oline.line_total) AS total_revenue
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+LEFT JOIN customer AS c
+	USING(customer_id)
+LEFT JOIN address AS add
+	USING(address_id)
+GROUP BY add.state
+ORDER BY total_revenue DESC; -- OR order by state ASC
+
+-- By Customer Age 
+
+-- Min age: 18 | Max Age: 29 | Average age: 23.5 years old
+SELECT  
+	MIN(AGE),
+	MAX(AGE), 
+	AVG(age) AS avg_age
+FROM
+(SELECT DATE_PART('year', AGE(NOW(),birthday)) as age FROM customer);
+
+-- By all ages
+SELECT 
+	DATE_PART('year', AGE(NOW(),c.birthday)) as customer_age,
+	SUM(oline.line_total) AS total_revenue
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+LEFT JOIN customer AS c
+	USING(customer_id)
+GROUP BY customer_age
+ORDER BY customer_age ASC;
+
+-- By age category
+SELECT 
+	CASE
+		WHEN DATE_PART('year', AGE(NOW(),c.birthday)) < 23 THEN 'Less than 23 yo'
+		WHEN DATE_PART('year', AGE(NOW(),c.birthday)) BETWEEN 23 AND 24 THEN '23-24 yo (AVG)'
+		ELSE 'Above 24 yo'
+	END as age_group,
+	SUM(oline.line_total) AS total_revenue
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+LEFT JOIN customer AS c
+	USING(customer_id)
+GROUP BY age_group;
 
 -- By Customer Gender
+SELECT 
+	c.gender,
+	SUM(oline.line_total) AS total_revenue
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+LEFT JOIN customer AS c
+	USING(customer_id)
+GROUP BY c.gender
+ORDER BY c.gender DESC;
 
 -- By Customer Membership Status
+SELECT 
+	CASE
+		WHEN c.is_member = TRUE THEN 'Members'
+		ELSE 'Non-members'
+	END as member_status,
+	SUM(oline.line_total) AS total_revenue
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+LEFT JOIN customer AS c
+	USING(customer_id)
+GROUP BY member_status;
 
 -- By Year
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	SUM(oline.line_total) AS total_revenue
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year
+ORDER BY year ASC;
+
+-- Yearly Growth Rate
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	ROUND((SUM(oline.line_total) - LAG(SUM(oline.line_total)) OVER (ORDER BY DATE_PART('year',o.order_date)))/ CAST(LAG(SUM(oline.line_total),1) OVER (ORDER BY DATE_PART('year',o.order_date)) AS DECIMAL)* 100,2) AS yearly_growth_rate
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year;
+
+-- Average Yearly Growth Rate
+SELECT ROUND(AVG(yearly_growth_rate),2) AS avg_yearly_growth_rate
+FROM
+(
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	ROUND((SUM(oline.line_total) - LAG(SUM(oline.line_total)) OVER (ORDER BY DATE_PART('year',o.order_date)))/ CAST(LAG(SUM(oline.line_total),1) OVER (ORDER BY DATE_PART('year',o.order_date)) AS DECIMAL)* 100,2) AS yearly_growth_rate
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year
+);
 
 -- By Months
+SELECT 
+	DATE_PART('year',o.order_date) AS year,
+	DATE_PART('month',o.order_date) AS month,
+	SUM(oline.line_total) AS total_revenue
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year, month
+ORDER BY year ASC, month ASC;
+
+-- Monthly Growth Rate
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	DATE_PART('month',o.order_date) AS month,
+	ROUND((SUM(oline.line_total) - LAG(SUM(oline.line_total)) OVER (ORDER BY DATE_PART('month',o.order_date)))/ CAST(LAG(SUM(oline.line_total),1) OVER (ORDER BY DATE_PART('month',o.order_date)) AS DECIMAL)* 100,2) AS monthly_growth_rate
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year,month;
+
+-- Average Monthly Growth Rate
+SELECT ROUND(AVG(monthly_growth_rate),2) AS avg_monthly_growth_rate
+FROM
+(
+SELECT 
+	DATE_PART('year',o.order_date) AS year, 
+	DATE_PART('month',o.order_date) As month,
+	ROUND((SUM(oline.line_total) - LAG(SUM(oline.line_total)) OVER (ORDER BY DATE_PART('month',o.order_date)))/ CAST(LAG(SUM(oline.line_total),1) OVER (ORDER BY DATE_PART('month',o.order_date)) AS DECIMAL)* 100,2) AS monthly_growth_rate
+FROM orders AS o
+LEFT JOIN orderline AS oline
+	USING(order_id)
+GROUP BY year,month
+);
 
 -- :: Average Order Value :: 
 
+-- Overall AOV
+SELECT 
+	ROUND(total_revenue/num_orders,2) AS avg_order_value
+FROM
+(
+SELECT 
+	COUNT(order_id) as num_orders,
+	SUM(line_total) as total_revenue
+FROM orderline
+);
+
 -- By Product Categories
+SELECT 
+	p_category,
+	ROUND(total_revenue/num_orders,2) AS avg_order_value
+FROM
+(
+SELECT
+	prod.category AS p_category,
+	COUNT(oline.order_id) AS num_orders,
+	SUM(oline.line_total) AS total_revenue
+FROM orderline AS oline
+LEFT JOIN product AS prod
+	USING(product_id)
+GROUP BY prod.category
+);
 
 -- By Product Sub-Categories
+SELECT 
+	p_sub_category,
+	ROUND(total_revenue/num_orders,2) AS avg_order_value
+FROM
+(
+SELECT
+	prod.sub_category As p_sub_category,
+	COUNT(oline.order_id) AS num_orders,
+	SUM(oline.line_total) AS total_revenue
+FROM orderline AS oline
+LEFT JOIN product AS prod
+	USING(product_id)
+GROUP BY prod.sub_category
+);
 
 -- By Customer Location
 
